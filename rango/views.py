4c1f90257e762e64a5d687f4dfd2c7404e2b6e21
Mohamed Madwar <2724529m@student.gla.ac.uk>
2023-02-07 01:05:00 +0000

    chapter 4 commit

--- rango/views.py
+++ rango/views.py
@@ -3,7 +3,12 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Rango says hey there partner!" + "(<a href='/rango/about/'>About</a>)")
+    context_dict ={'boldmessage':'Crunchy, creamy, cookie, candy, cupcake!'}  
+    return render(request, 'rango\index.html', context=context_dict)
+   # return HttpResponse("Rango says hey there partner!" + "(<a href='/rango/about/'>About</a>)")
      
 def about(request):
-    return HttpResponse('Rango says here is the about page'+ "(<a href='/rango'>index</a>)")
+    context_dict = {'boldmessage':'This tutorial has been put together by mohamed Madwar.'}
+    return render(request, 'rango/about.html', context=context_dict)
+    #return HttpResponse('Rango says here is the about page'+ "(<a href='/rango'>index</a>)") 
+    
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import include
 from django.urls import path
 from rango import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = "rango"
 
@@ -24,4 +26,4 @@ urlpatterns = [
     path('',views.index, name= 'index'),
     path('rango/', include('rango.urls')),
     path('admin/', admin.site.urls),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
